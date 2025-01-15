de0624954070270441d7440697d85d4d66137612
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2025-01-15 15:13:10 +0100

    Updates

--- src/components/Home/Heading/Heading.jsx
+++
@@ -1,7 +0,0 @@
-import "./Heading.css";
-const Heading = () => {
-  return (
-    <h1 className="app-header">Welcome to Good Day App, choose a Language</h1>
-  );
-};
-export default Heading;
--- src/pages/Greetings.jsx
+++
@@ -1,9 +0,0 @@
-import GreetingsComp from "../components/Greetings/GreetingsComp";
-const Greetings = () => {
-  return (
-    <>
-      <GreetingsComp />
-    </>
-  );
-};
-export default Greetings;
